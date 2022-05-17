6361a34a3be31497f7cece741056311f072759d7
Cameron <cameronevankirby@gmail.com>
2022-05-17 17:46:37 -0500

    :lipstick: Add about page

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,11 +1,71 @@
 import React from 'react'
 import StyledAbout from './About.styled'
+// Images
+import sergio from '../../assets/img/about-sergio.jpg'
 
 const About = () => {
 
     return (
         <StyledAbout>
-            Yo
+            <section className='about-landing'>
+                <h1 className='heading'>About.</h1>
+                <div className='landing-text'>
+                    <h2>Tecnica Environmental Services</h2>
+                    <p>Tecnica Environmental Services, Inc. is a full remediation  firm specializing in soil excavation, UST decommissioning, site-preparation and abatement services.</p>
+                    <p>For over 30 years, our family-owned professional environmental management and abatement firm has provided reliable, responsive and cost-effective solutions to our clients from government, industrial and private sectors.</p>
+                    <p>We collaborate with our clients to find customized solutions that utilize the latest technology, innovations and management techniques.   Throughout our history, we pride ourselves on our reputation of delivering superior service while respecting the health and safety of our employees, clients and environment.</p>
+                    <p>Our dedicated, highly trained team delivers our environmental customized services in the most effective and efficient manner.  Each member of our team brings a diverse, specialized background that meets all required professional standards.</p>
+                    <p>We are ready to continue to serve our clients with reliable and efficient environmental services for another 30 years.</p>
+                </div>
+            </section>
+            <section className='team'>
+                <div className='team-container'>
+                    <div className='member'>
+                        <div className='member-image'>
+                            <img src={sergio} alt=''/>
+                        </div>
+                        <div className='member-info'>
+                            <h3>Mr. Sergio Munoz</h3>
+                            <h4>CEO</h4>
+                            <p>Lorem ipsum dolor sit amet, consecteur adipiscing elit. Sagittis, est, ac proin amet mi nunc.</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='member-image'>
+                            <img src={sergio} alt=''/>
+                        </div>
+                        <div className='member-info'>
+                            <h3>Mr. Sergio Munoz</h3>
+                            <h4>CEO</h4>
+                            <p>Lorem ipsum dolor sit amet, consecteur adipiscing elit. Sagittis, est, ac proin amet mi nunc.</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='member-image'>
+                            <img src={sergio} alt=''/>
+                        </div>
+                        <div className='member-info'>
+                            <h3>Mr. Sergio Munoz</h3>
+                            <h4>CEO</h4>
+                            <p>Lorem ipsum dolor sit amet, consecteur adipiscing elit. Sagittis, est, ac proin amet mi nunc.</p>
+                        </div>
+                    </div>
+                    <div className='member'>
+                        <div className='member-image'>
+                            <img src={sergio} alt=''/>
+                        </div>
+                        <div className='member-info'>
+                            <h3>Mr. Sergio Munoz</h3>
+                            <h4>CEO</h4>
+                            <p>Lorem ipsum dolor sit amet, consecteur adipiscing elit. Sagittis, est, ac proin amet mi nunc.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='vision'>
+                <h2>Vision.</h2>
+                <p>The health and safety of all concerned parties is always our top priority.  Only the strictest safety and medical surveillance measures are utilized to ensure the protection of the workers, public and outside environment.  All our strengths give you, the client, unmatched quality and service.  It is not enough to simply offer a solution; it is our goal to provide the most effective and efficient solution for each encounter.</p>
+            </section>
         </StyledAbout>
     )
 }
--- frontend/src/pages/About/About.styled.js
+++ frontend/src/pages/About/About.styled.js
@@ -1,7 +1,85 @@
 import styled from 'styled-components'
 
 const StyledAbout = styled.div`
+    padding: 150px var(--gutter-width) 100px;
 
+    .about-landing {
+        display: flex;
+        flex-direction: row;
+        .heading {
+            width: 50%;
+            font-size: 7rem;
+            line-height: 7.5rem;
+            margin: 0 0 50px;
+        }
+
+        .landing-text {
+            width: 60%;
+            padding-left: 50px;
+            h2 {
+                margin: 0;
+                padding-top: 20px;
+                font-size: 2.3rem;
+                line-height: 2.8rem;
+            }
+        }
+    }
+    
+    .team {
+        padding-top: 100px;
+        .team-container {
+            padding: 50px 10px 15px;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            background-color: var(--off-white);
+            border-radius: 13px;
+
+            .member {
+                width: 50%;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: space-around;
+                padding-bottom: 35px;
+
+                .member-image {
+                    width: 35%;
+                    
+
+                    img {
+                        width: 100%;
+                        border-radius: 50%;
+                    }
+                }
+
+                .member-info {
+                    width: 50%;
+                    display: flex;
+                    flex-direction: column;
+
+                    h3, h4 {
+                        margin: 0;
+                    }
+                    h3 {
+                        font-size: 1.4rem;
+                    }
+                }
+            }
+        }
+    }
+
+    .vision {
+        padding-top: 50px;
+
+        h2 {
+            font-size: 4rem;
+        }
+
+        p {
+            width: 80%;
+        }
+    }
 `
 
 export default StyledAbout
